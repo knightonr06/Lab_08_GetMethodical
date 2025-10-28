6895905e3aa0b05a194cd89e8b62094ae18d9a7a
knightonr06 <knightonr06@gmail.com>
2025-10-28 00:11:43 -0400

    Update BirthDateTime.java

--- src/BirthDateTime.java
+++ src/BirthDateTime.java
@@ -1,16 +1,23 @@
 import java.util.Scanner;
 
-public class CheckOut {
+public class BirthDateTime {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double total = 0.0;
+        Scanner in = new Scanner(System.in);
+        int birthYear = 0;
+        int birthMonth = 0;
+        int birthDay = 0;
+        int birthHour = 0;
+        int birthMinute = 0;
 
-        do {
-            double price = SafeInput.getRangedDouble(sc, "Enter price of item ($0.50 - $10.00): ", 0.50, 10.00);
-            total += price;
-        } while (SafeInput.getYNConfirm(sc, "Do you have more items? (Y/N): "));
+        birthYear = SafeInput.getInt(in, "Enter your birth year (e.g., 1990): ");
+        birthMonth = SafeInput.getRangedInt(in, "Enter your birth month (1-12): ", 1, 12);
+        birthDay = SafeInput.getRangedInt(in, "Enter your birth day (1-31): ", 1, 31);
+        birthHour = SafeInput.getRangedInt(in, "Enter your birth hour (0-23): ", 0, 23);
+        birthMinute = SafeInput.getRangedInt(in, "Enter your birth minute (0-59): ", 0, 59);
 
-        System.out.printf("Total cost: $%.2f%n", total);
-        sc.close();
+        System.out.println("You were born on " + birthYear + "-" + String.format("%02d", birthMonth) + "-" +
+                String.format("%02d", birthDay) + " at " + String.format("%02d", birthHour) + ":" +
+                String.format("%02d", birthMinute) + ".");
     }
 }
+
