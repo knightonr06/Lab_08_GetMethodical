1a6db123537ebf84a8b443e29d98704789f85974
knightonr06 <knightonr06@gmail.com>
2025-10-28 00:24:47 -0400

    Update FavNumbers.java

--- src/FavNumbers.java
+++ src/FavNumbers.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class FavNumbers {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int favInt = 0;
+        double favDouble = 0.0;
+
+        favInt = SafeInput.getInt(in, "Enter your favorite integer: ");
+        favDouble = SafeInput.getDouble(in, "Enter your favorite double: ");
+
+        System.out.println("Your favorite integer is: " + favInt);
+        System.out.println("Your favorite double is: " + favDouble);
+    }
 }
