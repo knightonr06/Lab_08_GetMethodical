d8dd8488acef70f46d017845fc9090f4dfa04e73
knightonr06 <knightonr06@gmail.com>
2025-10-28 00:10:31 -0400

    Update BirthDateTime.java

--- src/BirthDateTime.java
+++ src/BirthDateTime.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class BirthDateTime {
+public class CheckOut {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double total = 0.0;
+
+        do {
+            double price = SafeInput.getRangedDouble(sc, "Enter price of item ($0.50 - $10.00): ", 0.50, 10.00);
+            total += price;
+        } while (SafeInput.getYNConfirm(sc, "Do you have more items? (Y/N): "));
+
+        System.out.printf("Total cost: $%.2f%n", total);
+        sc.close();
+    }
 }
